a76afd62905821cd7c3e3621a020047972417949
itreverie <brendadhk@gmail.com>
2017-08-02 19:40:21 -0400

    Adding the binding statements

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,13 +2,48 @@ import React , {PropTypes} from 'react';
 
 class CoursesPage extends React.Component
 {
+  constructor (props, context)
+  {
+
+    //constructor that initializes the state
+    super(props, context);
+    this.state = {
+                  course: { title: null}
+                 };
+    //Adding bind statements for our functions
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+
+  }
+
+  //Adding functions
+  onTitleChange(event){
+    const course=this.state.course;
+    course.title = event.target.value;
+    this.setState({course:course});
+  }
+
+  onClickSave()
+  {
+    alert(`Saving ${this.state.course.title}`);
+  }
+
  render(){
    return (
      <div>
-       <h1>Courses Page</h1>
+       <h1>Courses</h1>
+       <h2>Add Courses</h2>
+       <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}/>
+       <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave}/>
      </div>
    );
  }
-};
+}
 
 export default CoursesPage;
